c559bec972891fa06df370c30ba78d0e19523169
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-26 21:57:16 +0530

    bugfix for making it work

--- run_backup.sh
+++ run_backup.sh
@@ -8,7 +8,7 @@ echo running script $PWD/$0
 echo DB file path is ${NEO_DB_MNT_PATH}
 BACKUP_NEO4J_DIR_HOSTPATH=${NEO_DB_MNT_PATH}/backup/
 echo "starting backup at" `date`
-mv /backup/neo4j_dump.dump /backup/neo4j_dump.dump_bkp
+mv ${BACKUP_NEO4J_DIR_HOSTPATH}/neo4j_dump.dump ${BACKUP_NEO4J_DIR_HOSTPATH}/neo4j_dump.dump_bkp
 docker stop ${NEO4J_BKP_SVR_NAME}
 echo "stopped server for backup at" `date`
 docker run --rm  -i --name ${NEO4J_BKP_TMP_CONTAINER_NAME} -v ${NEO_DB_MNT_PATH}/data:/data -v ${NEO_DB_MNT_PATH}/backup:/backup neo4j:4.0.1 /bin/bash -c "bin/neo4j-admin dump --database=neo4j --to=/backup/neo4j_dump.dump"

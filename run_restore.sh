081407c8b8707fdec8cda0ead7fd732705302b3e
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-27 05:27:51 +0530

    Enhanced log while restore

--- run_restore.sh
+++ run_restore.sh
@@ -10,9 +10,9 @@ RESTORE_NEO4J_DIR_HOSTPATH=${NEO_DB_MNT_PATH}/restore/
 echo "starting restore at" `date`
 cp ${RESTORE_NEO4J_DIR_HOSTPATH}/neo4j_dump.dump ${RESTORE_NEO4J_DIR_HOSTPATH}/neo4j_dump.dump_bkp
 docker stop ${NEO4J_SVR_NAME}
-echo "stopped server for restore at" `date`
+echo "stopped ${NEO4J_SVR_NAME} server for restore at" `date`
 docker run --rm  -i --name ${NEO4J_TMP_CONTAINER_NAME} -v ${NEO_DB_MNT_PATH}/data:/data -v ${NEO_DB_MNT_PATH}/restore:/restore neo4j:4.0.1 /bin/bash -c "bin/neo4j-admin load --database=neo4j --from=/restore/neo4j_dump.dump --force"
-echo "finished restore at" `date`
+echo "finished ${NEO4J_SVR_NAME} restore at" `date`
 chown -R neo4jadmin ${NEO_DB_MNT_PATH}/data
 chgrp -R neo4jadmin ${NEO_DB_MNT_PATH}/data
 sleep 2

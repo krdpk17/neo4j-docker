4a1e1d24aec15a43c1df9652a6b983e2df946b91
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-27 08:36:34 +0530

    Dump file name is changed and made configurable [Not tested]

--- run_backup.sh
+++ run_backup.sh
@@ -8,10 +8,10 @@ echo running script $PWD/$0
 echo DB file path is ${NEO_DB_MNT_PATH}
 BACKUP_NEO4J_DIR_HOSTPATH=${NEO_DB_MNT_PATH}/backup/
 echo "starting backup at" `date`
-mv ${BACKUP_NEO4J_DIR_HOSTPATH}/neo4j_dump.dump ${BACKUP_NEO4J_DIR_HOSTPATH}/neo4j_dump.dump_bkp
+mv ${BACKUP_NEO4J_DIR_HOSTPATH}/${NEO4J_DUMP_FILE_NAME} ${BACKUP_NEO4J_DIR_HOSTPATH}/${NEO4J_DUMP_FILE_NAME}_bkp
 docker stop ${NEO4J_SVR_NAME}
 echo "stopped ${NEO4J_SVR_NAME} server for backup at" `date`
-docker run --rm  -i --name ${NEO4J_TMP_CONTAINER_NAME} -v ${NEO_DB_MNT_PATH}/data:/data -v ${NEO_DB_MNT_PATH}/backup:/backup neo4j:4.0.1 /bin/bash -c "bin/neo4j-admin dump --database=neo4j --to=/backup/neo4j_dump.dump"
+docker run --rm  -i --name ${NEO4J_TMP_CONTAINER_NAME} -v ${NEO_DB_MNT_PATH}/data:/data -v ${NEO_DB_MNT_PATH}/backup:/backup neo4j:4.0.1 /bin/bash -c "bin/neo4j-admin dump --database=neo4j --to=/backup/${NEO4J_DUMP_FILE_NAME}"
 echo "finished backup at" `date`
 chown -R neo4jadmin ${NEO_DB_MNT_PATH}/data
 chgrp -R neo4jadmin ${NEO_DB_MNT_PATH}/data
--- run_restore.sh
+++ run_restore.sh
@@ -8,10 +8,10 @@ echo running script $PWD/$0
 echo DB file path is ${NEO_DB_MNT_PATH}
 RESTORE_NEO4J_DIR_HOSTPATH=${NEO_DB_MNT_PATH}/restore/
 echo "starting restore at" `date`
-cp ${RESTORE_NEO4J_DIR_HOSTPATH}/neo4j_dump.dump ${RESTORE_NEO4J_DIR_HOSTPATH}/neo4j_dump.dump_bkp
+cp ${RESTORE_NEO4J_DIR_HOSTPATH}/${NEO4J_DUMP_FILE_NAME} ${RESTORE_NEO4J_DIR_HOSTPATH}/${NEO4J_DUMP_FILE_NAME}_bkp
 docker stop ${NEO4J_SVR_NAME}
 echo "stopped ${NEO4J_SVR_NAME} server for restore at" `date`
-docker run --rm  -i --name ${NEO4J_TMP_CONTAINER_NAME} -v ${NEO_DB_MNT_PATH}/data:/data -v ${NEO_DB_MNT_PATH}/restore:/restore neo4j:4.0.1 /bin/bash -c "bin/neo4j-admin load --database=neo4j --from=/restore/neo4j_dump.dump --force"
+docker run --rm  -i --name ${NEO4J_TMP_CONTAINER_NAME} -v ${NEO_DB_MNT_PATH}/data:/data -v ${NEO_DB_MNT_PATH}/restore:/restore neo4j:4.0.1 /bin/bash -c "bin/neo4j-admin load --database=neo4j --from=/restore/${NEO4J_DUMP_FILE_NAME} --force"
 echo "finished ${NEO4J_SVR_NAME} restore at" `date`
 chown -R neo4jadmin ${NEO_DB_MNT_PATH}/data
 chgrp -R neo4jadmin ${NEO_DB_MNT_PATH}/data

165b5810290344a96945bec3fd52da82685527b0
Deepak Kumar <33544658+krdpk17@users.noreply.github.com>
2020-07-12 08:34:32 +0530

    removed backup of the restore file as it is read-only

--- run_restore.sh
+++ run_restore.sh
@@ -8,7 +8,6 @@ echo running script $PWD/$0
 echo DB file path is ${NEO_DB_MNT_PATH}
 RESTORE_NEO4J_DIR_HOSTPATH=${NEO_DB_MNT_PATH}/restore/
 echo "starting restore at" `date`
-cp ${RESTORE_NEO4J_DIR_HOSTPATH}/${NEO4J_DUMP_FILE_NAME} ${RESTORE_NEO4J_DIR_HOSTPATH}/${NEO4J_DUMP_FILE_NAME}_bkp
 docker stop ${NEO4J_SVR_NAME}
 echo "stopped ${NEO4J_SVR_NAME} server for restore at" `date`
 docker run --rm  -i --name ${NEO4J_TMP_CONTAINER_NAME} -v ${NEO_DB_MNT_PATH}/data:/data -v ${NEO_DB_MNT_PATH}/restore:/restore neo4j:4.0.1 /bin/bash -c "bin/neo4j-admin load --database=neo4j --from=/restore/${NEO4J_DUMP_FILE_NAME} --force"
